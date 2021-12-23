2566a69b10da957d5f2474e8e386416d05968ed0
mazalmekonen <87172425+mazalmekonen@users.noreply.github.com>
2021-12-23 10:23:56 +0200

    Update employees.component.ts

--- employee-app/src/app/employees/employees.component.ts
+++ employee-app/src/app/employees/employees.component.ts
@@ -3,6 +3,8 @@ import { Employee } from '../employee';
 //import {EMPLOYEES } from '../list-employees';
 import { EmployeeService } from '../employee.service';
 import { MessageService } from '../message.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -18,17 +20,23 @@ export class EmployeesComponent implements OnInit {
   employees: Employee[] = [];
 
   selectedEmployee?: Employee;
+  
   onSelect(employee: Employee): void {
   this.selectedEmployee = employee;
-  this.messageService.add(`Employee Component: Selected employee id=${employee.id}`);
+  this.messageService.add(`Employee Component message: Selected employee id=${employee.id}`);
 }
   
-  constructor(private employeeService: EmployeeService, private messageService: MessageService) {}
+constructor(
+  private route: ActivatedRoute,
+  private employeeService: EmployeeService,
+  private location: Location,
+  private messageService: MessageService
+) {}
 
   getEmployees(): void {
-      //this.employees = this.employeeService.getEmployees();
+     //this.employees = this.employeeService.getEmployees();
     this.employeeService.getEmployees()
-    .subscribe(employees => this.employees = employees);
+      .subscribe(employees => this.employees = employees);
     }    
 
   ngOnInit(): void {
